5de1a1d7987fd49f0dc38d2ea85ee75aa0644770
ilya.michlin <ilya.michlin@lemonade.com>
2024-04-11 12:25:33 +0300

    parse_commit_message

---
+++ release/parse_commit_message.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+import semantic_release
+import git
+
+
+class MyParserOptions(semantic_release.ParserOptions):
+    def __init__(self, message_prefix: str, **_: Any) -> None:
+        super().__init__(**_)
+        self.prefix = message_prefix * 2
+
+
+class MyCommitParser(
+    semantic_release.CommitParser[semantic_release.ParseResult, MyParserOptions]
+):
+    def parse(self, commit: git.objects.commit.Commit) -> semantic_release.ParseResult:
+        return semantic_release.ParsedCommit(bump=semantic_release.LevelBump.PATCH, type="feat", scope="", descriptions=[""], breaking_descriptions=[""], commit=commit)
