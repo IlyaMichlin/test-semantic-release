fdec103ee2129c818c1c076ff22377d0e410f613
ilya.michlin <ilya.michlin@lemonade.com>
2024-04-11 14:16:01 +0300

    commit_parser = 'release.changelog_parser.parse_commit_message'

--- release/parse_commit_message.py
+++ release/parse_commit_message.py
@@ -14,6 +14,7 @@ class MyCommitParser(
     semantic_release.CommitParser[semantic_release.ParseResult, MyParserOptions]
 ):
     def parse(self, commit: git.objects.commit.Commit) -> semantic_release.ParseResult:
+        print("commit.message: ", commit.message)
         return semantic_release.ParsedCommit(
             bump=semantic_release.LevelBump.PATCH,
             type="feat",
