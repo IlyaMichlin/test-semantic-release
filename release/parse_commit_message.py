64ab31cb24f4fc826c910b7784c0b1d553713050
ilya.michlin <ilya.michlin@lemonade.com>
2024-04-11 15:18:57 +0300

    fixes

--- release/parse_commit_message.py
+++ release/parse_commit_message.py
@@ -1,18 +1,15 @@
-from typing import Any
-
 import semantic_release
 import git
 
 
-class MyParserOptions(semantic_release.ParserOptions):
-    def __init__(self, message_prefix: str, **_: Any) -> None:
-        super().__init__(**_)
-        self.prefix = message_prefix * 2
+class MyParserOptions(semantic_release.ParserOptions): ...
 
 
 class MyCommitParser(
     semantic_release.CommitParser[semantic_release.ParseResult, MyParserOptions]
 ):
+    parser_options = MyParserOptions
+
     def parse(self, commit: git.objects.commit.Commit) -> semantic_release.ParseResult:
         print("commit.message: ", commit.message)
         return semantic_release.ParsedCommit(
