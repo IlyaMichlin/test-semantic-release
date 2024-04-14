ebc3f3c361d34963ea787aecb363ba32d58b1824
ilya.michlin <ilya.michlin@lemonade.com>
2024-04-14 10:20:31 +0300

    python-semantic-release/python-semantic-release@9.4.2

--- release/parse_commit_message.py
+++ release/parse_commit_message.py
@@ -1,21 +1,21 @@
-import semantic_release
+from semantic_release import ParsedCommit, LevelBump, ParserOptions, CommitParser, ParseResult
 import git
 
 
-class MyParserOptions(semantic_release.ParserOptions): ...
+class MyParserOptions(ParserOptions): ...
 
 
 class MyCommitParser(
-    semantic_release.CommitParser[semantic_release.ParseResult, MyParserOptions]
+    CommitParser[ParseResult, MyParserOptions]
 ):
     parser_options = MyParserOptions
 
-    def parse(self, commit: git.objects.commit.Commit) -> semantic_release.ParseResult:
-        return semantic_release.ParsedCommit(
-            bump=semantic_release.LevelBump.PATCH,
+    def parse(self, commit: git.objects.commit.Commit) -> ParseResult:
+        return ParsedCommit(
+            bump=LevelBump.PATCH,
             type="patch",
-            scope="scope",
-            descriptions=["descriptions"],
-            breaking_descriptions=["breaking_descriptions"],
+            scope="",
+            descriptions=[""],
+            breaking_descriptions=[""],
             commit=commit
         )
